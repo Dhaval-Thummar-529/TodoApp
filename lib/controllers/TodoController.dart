967d82995fae6fed32c890f2d1d16e4c5280e3a7
Dhaval-Thummar-529 <190304105062@paruluniversity.ac.in>
2023-07-25 23:05:53 +0530

    added CheckBox Logic

--- lib/controllers/TodoController.dart
+++ lib/controllers/TodoController.dart
@@ -1,11 +1,11 @@
 import 'package:get/get.dart';
+import 'package:todo_app/controllers/ActiveController.dart';
 import 'package:todo_app/controllers/MyTodoAppController.dart';
 
 import '../Model/TodoModel.dart';
 import '../Service/DatabaseHandler.dart';
 
-class TodoController extends GetxController{
-
+class TodoController extends GetxController {
   //To-do list from database
   late List<TodoModel> todoList;
 
@@ -14,6 +14,11 @@ class TodoController extends GetxController{
 
   //Database Handler
   late DatabaseHandler handler = DatabaseHandler();
+
+  //Observable variable for UpdatingTask
+  var isTaskUpdating = false.obs;
+
+  var checkBoxValueList = List.generate(0, (index) => false).obs;
 
   @override
   void onInit() {
@@ -26,7 +31,8 @@ class TodoController extends GetxController{
     isLoading(true);
     try {
       todoList = await handler.retrieveTodoByStatus("Todo");
-    } catch(e){
+      checkBoxValueList = List.generate(todoList.length, (index) => false).obs;
+    } catch (e) {
       print(e);
       isLoading(false);
     } finally {
@@ -34,7 +40,7 @@ class TodoController extends GetxController{
     }
   }
 
-  getDateDifference(String startDate, String endDate){
+  getDateDifference(String startDate, String endDate) {
     var sDate = DateTime.now();
     var eDate = DateTime.parse(endDate);
     var difference = eDate.difference(sDate).inDays;
@@ -42,4 +48,29 @@ class TodoController extends GetxController{
     return difference;
   }
 
+  updateTaskToActive(int index) {
+    /*isTaskUpdating(true);*/
+    Future.delayed(const Duration(seconds: 3), () {
+      try {
+        TodoModel activeModel = TodoModel(
+          id: todoList[index].id,
+            title: todoList[index].title,
+            description: todoList[index].description,
+            status: "active",
+            startDate: todoList[index].startDate,
+            modifiedDate: todoList[index].modifiedDate,
+            endDate: todoList[index].endDate);
+        handler.updateTodoStatus(activeModel);
+        var controller = Get.find<ActiveController>();
+        controller.fetchTodo();
+        /*isTaskUpdating(false);*/
+      } catch (e) {
+        print("Exception : $e");
+        /*isTaskUpdating(false);*/
+      } finally {
+        /*isTaskUpdating(false);*/
+        fetchTodo();
+      }
+    });
+  }
 }
--- lib/screens/ToDo.dart
+++ lib/screens/ToDo.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:todo_app/Model/TodoModel.dart';
 import 'package:todo_app/controllers/TodoController.dart';
 
 import '../customWidgets/customCheckBox.dart';
@@ -51,73 +52,98 @@ class TodoListTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-        color: controller.getDateDifference(controller.todoList[index].startDate!, controller.todoList[index].endDate!)+1 <= 1 ? Colors.red.withOpacity(0.8) : controller.getDateDifference(controller.todoList[index].startDate!, controller.todoList[index].endDate!)+1 <= 2 ? Colors.orangeAccent.withOpacity(0.8) : Colors.green.withOpacity(0.8),
+        color: controller.getDateDifference(
+                        controller.todoList[index].startDate!,
+                        controller.todoList[index].endDate!) +
+                    1 <=
+                1
+            ? Colors.red.withOpacity(0.8)
+            : controller.getDateDifference(
+                            controller.todoList[index].startDate!,
+                            controller.todoList[index].endDate!) +
+                        1 <=
+                    2
+                ? Colors.orangeAccent.withOpacity(0.8)
+                : Colors.green.withOpacity(0.8),
         elevation: 10,
         shape:
             RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    width: 150,
-                    child: Text(
-                      controller.todoList[index].title,
-                      style: const TextStyle(
-                        overflow: TextOverflow.ellipsis,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 150,
-                    child: Text(
-                      controller.todoList[index].description,
-                      style: const TextStyle(
-                        overflow: TextOverflow.ellipsis,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Column(
+        child: Obx(
+          () => controller.isTaskUpdating.value
+              ? const Center(child: CircularProgressIndicator())
+              : Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 10.0, vertical: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text(
-                        "Start Date : ${controller.todoList[index].startDate}",
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w400,
-                        ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            width: 150,
+                            child: Text(
+                              controller.todoList[index].title,
+                              style: const TextStyle(
+                                overflow: TextOverflow.ellipsis,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 150,
+                            child: Text(
+                              controller.todoList[index].description,
+                              style: const TextStyle(
+                                overflow: TextOverflow.ellipsis,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                      Text(
-                        "End Date   : ${controller.todoList[index].endDate}",
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w400,
-                        ),
+                      Row(
+                        children: [
+                          Column(
+                            children: [
+                              Text(
+                                "Start Date : ${controller.todoList[index].startDate}",
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                              Text(
+                                "End Date   : ${controller.todoList[index].endDate}",
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Checkbox(value: controller.checkBoxValueList[index], onChanged: (val){
+                            if (val!) {
+                              controller.checkBoxValueList[index] = true;
+                              controller.updateTaskToActive(index);
+                            }
+                          },
+                            activeColor: Colors.blue,shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(5.0),
+                                side: const BorderSide(color: Colors.red)
+                            ),)
+                          /*CustomCheckBox(onChange: (val) {
+                            if (val!) {
+                              controller.updateTaskToActive(index);
+                            }
+                          })*/
+                        ],
                       ),
                     ],
                   ),
-                  CustomCheckBox(onChange: (val) {
-                    if (val!) {
-                      print(val);
-                    } else {
-                      print(val);
-                    }
-                  })
-                ],
-              ),
-            ],
-          ),
+                ),
         ));
   }
 }
